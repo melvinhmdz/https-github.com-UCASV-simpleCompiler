51cb4f27eccc33a9f2be8b730745ddf09a68fd47
melvinhmdz <35278692+melvinhmdz@users.noreply.github.com>
2018-01-10 18:32:23 -0600

    Update backend.c
    Resultado de operacion en variable global, 'value'

--- backend.c
+++ backend.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "parser.h"
 #include "backend.h"
-
+int value;
 static void Gen_codigo_expresion(Expresion *expr) {
   switch(expr->tipo) {
     case 'D':
@@ -14,11 +14,11 @@ static void Gen_codigo_expresion(Expresion *expr) {
       switch (expr->oper) {
         case '+':
           printf("ADD\n");
-          printf("%d\n",expr->izquierda->valor + expr->derecha->valor);
+          value = expr->izquierda->valor + expr->derecha->valor;
           break;
         case '*':
           printf("MULT\n");
-          printf("%d\n",expr->izquierda->valor * expr->derecha->valor);
+          value = expr->izquierda->valor * expr->derecha->valor;
           break;
       }
       break;
@@ -28,5 +28,5 @@ static void Gen_codigo_expresion(Expresion *expr) {
 
 void Procesar(nodo_AST *codigoi){
   Gen_codigo_expresion(codigoi);
-  printf("PRINT\n");
+  printf("PRINT %d\n",value);
 }
